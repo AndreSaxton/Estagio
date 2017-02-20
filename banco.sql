8387663acbc3d5ef466baeb132fa2ea14fb90529
AndreSaxton <andre_saxton@hotmail.com>
2017-02-20 16:28:20 -0300

    Add files via upload

---
+++ banco.sql
@@ -0,0 +1,17 @@
+Create database  if not exists teste DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+Use teste;
+
+drop table if exists mercadoria;
+Create table mercadoria
+(	
+	codigo int not null auto_increment,  
+	codmer int not null,
+	tipo varchar (45) not null,
+	nome varchar (40) not null,
+    quant int not null,
+	preco float(9,2) not null,
+    negocio int not null,
+	constraint pk_mercadoria
+		primary key(codigo)
+);
