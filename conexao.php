644e707bb132322b2541338fb1725aec7606c8d8
AndreSaxton <andre_saxton@hotmail.com>
2017-02-22 02:13:32 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,5 +1,7 @@
 <?php
-$conexao = new mysqli('localhost', 'root','', 'teste'); 
+//$conexao = new mysqli('localhost', 'root','', 'teste'); 
+$conexao = new mysqli('mysql.hostinger.com.br', 'u770413803_teste','fornazieri', 'u770413803_teste'); 
+//93.188.160.89
 //Variavel = new mysqli("servidor", "usuario", "senha", "banco") or die("Erro ao conectar");
 	if (!$conexao)
 	{ 
