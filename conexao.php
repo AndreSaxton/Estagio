8514800ef35b0498a7c643df7a980677929dd835
AndreSaxton <andre_saxton@hotmail.com>
2017-09-05 20:39:59 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -2,7 +2,7 @@
 //$conexao = new mysqli('localhost', 'root','', 'teste'); 
 //$conexao = new mysqli('mysql.hostinger.com.br', 'u770413803_teste','fornazieri', 'u770413803_teste');
 //Database = ", "saxton", "{your_password_here}");
-$conexao = new mysqli('tcp:serverpresentacao1309.database.windows.net', 'saxton','apresentac@0', 'dbapresentacao1309'); 
+$conexao = new mysqli('serverpresentacao1309.database.windows.net', 'saxton','apresentac@0', 'dbapresentacao1309'); 
 //93.188.160.89
 //Variavel = new mysqli("servidor", "usuario", "senha", "banco") or die("Erro ao conectar");
 	if (!$conexao)
